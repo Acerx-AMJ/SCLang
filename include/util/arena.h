5d0935db5a47c6745355c230be416e134861275a
Acerx-AMJ <ajm.acerx@gmail.com>
2025-12-21 20:12:55 +0200

    Added arena allocator.

---
+++ include/util/arena.h
@@ -0,0 +1,27 @@
+#ifndef UTIL_ARENA_H
+#define UTIL_ARENA_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct {
+    char *base;
+    size_t size;
+    size_t used;
+} Arena;
+
+// Initialize 
+
+Arena Arena_init(size_t size);
+
+// Allocate
+
+void* Arena_alloc(Arena *arena, size_t size);
+void* Arena_allocAligned(Arena *arena, size_t size, size_t alignment);
+
+// Free
+
+void Arena_reset(Arena *arena);
+void Arena_free(Arena *arena);
+
+#endif
---
+++ src/util/arena.c
@@ -0,0 +1,52 @@
+#include "util/arena.h"
+#include <stdlib.h>
+
+// Initialize
+
+Arena Arena_init(size_t size) {
+   Arena arena;
+   arena.base = malloc(size);
+   arena.size = size;
+   arena.used = 0;
+   return arena;
+}
+
+// Allocate
+
+void* Arena_alloc(Arena *arena, size_t size) {
+   if (arena->used + size > arena->size) {
+      return NULL;
+   }
+
+   void *pointer = arena->base + arena->used;
+   arena->used += size;
+   return pointer;
+}
+
+void* Arena_allocAligned(Arena *arena, size_t size, size_t alignment) {
+   size_t current = (size_t)(arena->base + arena->used);
+   size_t aligned = (current + (alignment - 1)) & ~(alignment - 1);
+   size_t offset = aligned - (size_t)arena->base;
+
+   if (offset + size > arena->size) {
+      return NULL;
+   }
+
+   arena->used = offset + size;
+   return (void*)aligned;
+}
+
+// Free
+
+void Arena_reset(Arena *arena) {
+   arena->used = 0;
+}
+
+void Arena_free(Arena *arena) {
+   if (arena->base) {
+      free(arena->base);
+      arena->base = NULL;
+      arena->size = 0;
+      arena->used = 0;
+   }
+}
