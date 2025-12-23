92a56124eb08187b4a5852c503f3e7f097d69d6c
Acerx-AMJ <ajm.acerx@gmail.com>
2025-12-23 19:37:52 +0200

    Moved code to a string view.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
+#include "util/arena.h"
 #include "util/fileio.h"
 #include "util/string.h"
+#include "util/view.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,9 +12,13 @@ int main(int argc, char *argv[]) {
    }
 
    String input = String_copyCstringNew(argv[1]);
-   String code = readFileIfGiven(&input);
+   String codeString = readFileIfGiven(&input);
+
+   Arena arena = Arena_init(1024 * 1024);
+   View code = View_takeoverNew(&arena, &codeString);
+
    printf("'%s' - %lu\n", code.base, code.size);
 
-   String_free(&code);
+   Arena_free(&arena);
    return 0;
 }
